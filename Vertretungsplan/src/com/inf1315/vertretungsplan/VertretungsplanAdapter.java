6878e23c995e2a26eab61c1370cb1f9b7d93ada8
skyrising <simon.wanner@gmx.net>
2014-01-19 00:00:12 +0100

    Fixed padding of list

--- Vertretungsplan/src/com/inf1315/vertretungsplan/VertretungsplanAdapter.java
+++ Vertretungsplan/src/com/inf1315/vertretungsplan/VertretungsplanAdapter.java
@@ -21,7 +21,8 @@ public class VertretungsplanAdapter extends ArrayAdapter<ReplacementObject>
     public View getView(int position, View convertView, ViewGroup parent)
     {
 	TextView tv = new TextView(getContext());
-	ReplacementObject o = getItem(position);	
+	ReplacementObject o = getItem(position);
+	tv.setPadding(10, 4, 8, 4);
 	tv.append(o.lesson + ". " + o.teacher + " -> " + o.comment + (o.replacement.trim().length() > 0 ? " (" + o.replacement.trim() + ")" : "") + (o.room.length() > 0 ? " in " + o.room : "") + "\n");
 	//tv.append(o.lesson + ". " + Teachers.expand(o.teacher) + " -> " + o.comment + (o.replacement.trim().length() > 0 ? " (" + o.replacement.trim() + ")" : "") + (o.room.length() > 0 ? " in " + o.room : "") + "\n");
 	if (o.addition)
