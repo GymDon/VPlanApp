8aee7b487627f9f10af1a01fc04f9e3553badf71
skyrising <simon.wanner@gmx.net>
2014-01-18 17:59:51 +0100

    Added OtherObject.java

---
+++ Vertretungsplan/src/com/inf1315/vertretungsplan/api/OtherObject.java
@@ -0,0 +1,34 @@
+package com.inf1315.vertretungsplan.api;
+
+import org.json.*;
+
+public class OtherObject extends ApiResult
+{
+	public final int id;
+	public final OtherType type;
+	public final String name;
+	public final String lesson;
+	public final String comment;
+	public final long timestamp;
+	public final long timestampUpdate;
+	public final boolean addition;
+	public final boolean isToday;
+	
+	public OtherObject(JSONObject obj) throws JSONException
+	{
+		id = obj.getInt("id");
+		type = OtherType.valueOf(obj.getString("type"));
+		name = obj.getString("name");
+		lesson = obj.getString("lesson");
+		comment = obj.getString("comment");
+		timestamp = obj.getLong("timestamp");
+		timestampUpdate = obj.getLong("timestamp_update");
+		addition = obj.getBoolean("addition");
+		isToday = obj.getBoolean("is_today");
+	}
+	
+	public static enum OtherType
+	{
+		T,G,R,S,A,N;
+	}
+}
