96f21784c6cd76d63f23d6a3b5a1bcb879ad2890
LeMuecke <simonklimaschka@t-online.de>
2014-01-18 19:11:02 +0100

    Added Settings and Infopage

--- Vertretungsplan/src/com/inf1315/vertretungsplan/LoginActivity.java
+++ Vertretungsplan/src/com/inf1315/vertretungsplan/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.inf1315.vertretungsplan;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,16 +19,18 @@ public class LoginActivity extends Activity {
 	private EditText usernameEditText;
 	private EditText passwordEditText;
 	public static Dialog loadingDialog;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_login);
-		
-		//Initalize variables
+
+		// Initalize variables
 		usernameEditText = (EditText) findViewById(R.id.username_EditText);
 		passwordEditText = (EditText) findViewById(R.id.password_EditText);
+		
+		PreferenceManager.setDefaultValues(this, R.layout.preferences, false);
 	}
 
 	@Override
@@ -35,17 +40,32 @@ public class LoginActivity extends Activity {
 		return true;
 	}
 	
-	public void loginButtonOnClick (View v) {
-		//TODO: Do login
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			Intent startSettings = new Intent(this, SettingsActivity.class);
+			startActivity(startSettings);
+			return true;
+		case R.id.action_info:
+		    Intent startInfo = new Intent(this, InfoActivity.class);
+		    startActivity(startInfo);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void loginButtonOnClick(View v) {
+		// TODO: Do login
 		String username = usernameEditText.getText().toString();
 		String password = passwordEditText.getText().toString();
 
 		loadingDialog = ProgressDialog.show(this, "", "Loading", true);
 		loadingDialog.show();
-		
-		//TODO: Remove debug
-		Log.d("Login", "Username : "+username+" Password : "+ password);
-		Intent intent = new Intent(this,PlanActivity.class);
+
+		// TODO: Remove debug
+		Log.d("Login", "Username : " + username + " Password : " + password);
+		Intent intent = new Intent(this, PlanActivity.class);
 		startActivity(intent);
 	}
 
--- Vertretungsplan/src/com/inf1315/vertretungsplan/PlanActivity.java
+++ Vertretungsplan/src/com/inf1315/vertretungsplan/PlanActivity.java
@@ -8,225 +8,262 @@ import java.util.concurrent.ExecutionException;
 import com.inf1315.vertretungsplan.api.*;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.*;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.*;
 import android.widget.Toast;
 
-public class PlanActivity extends FragmentActivity implements ActionBar.TabListener
-{
+public class PlanActivity extends FragmentActivity implements
+		ActionBar.TabListener {
 
-    PlanPagerAdapter planPagerAdapter;
-    ViewPager viewPager;
-    int ticker = 0;
-    List<TickerObject> tickers = new ArrayList<TickerObject>();
+	SharedPreferences sharedPref;
+	Boolean tickerToast;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_plan);
+	PlanPagerAdapter planPagerAdapter;
+	ViewPager viewPager;
+	int ticker = 0;
+	List<TickerObject> tickers = new ArrayList<TickerObject>();
 
-	// Set up the action bar.
-	final ActionBar actionBar = getActionBar();
-	actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-	// Show the Up button in the action bar.
-	actionBar.setDisplayHomeAsUpEnabled(true);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_plan);
 
-	// Create the adapter that will return a fragment for each of the three
-	// primary sections of the app.
-	planPagerAdapter = new PlanPagerAdapter(getSupportFragmentManager());
+		getPreferences();
 
-	// Set up the ViewPager with the sections adapter.
-	viewPager = (ViewPager) findViewById(R.id.plan_pager);
-	viewPager.setAdapter(planPagerAdapter);
+		// Set up the action bar.
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		// Show the Up button in the action bar.
+		actionBar.setDisplayHomeAsUpEnabled(true);
 
-	// When swiping between different sections, select the corresponding
-	// tab. We can also use ActionBar.Tab#select() to do this if we have
-	// a reference to the Tab.
-	viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
-	{
-	    @Override
-	    public void onPageSelected(int position)
-	    {
-		actionBar.setSelectedNavigationItem(position);
-	    }
-	});
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the app.
+		planPagerAdapter = new PlanPagerAdapter(getSupportFragmentManager());
 
-	// For each of the sections in the app, add a tab to the action bar.
-	for (int i = 0; i < planPagerAdapter.getCount(); i++)
-	{
-	    // Create a tab with text corresponding to the page title defined by
-	    // the adapter. Also specify this Activity object, which implements
-	    // the TabListener interface, as the callback (listener) for when
-	    // this tab is selected.
-	    actionBar.addTab(actionBar.newTab().setText(planPagerAdapter.getPageTitle(i)).setTabListener(this));
-	}
-	loadData();
-    }
+		// Set up the ViewPager with the sections adapter.
+		viewPager = (ViewPager) findViewById(R.id.plan_pager);
+		viewPager.setAdapter(planPagerAdapter);
 
-    private void loadData()
-    {
-	final List<AsyncTask<?, ?, ?>> tasks = new ArrayList<AsyncTask<?, ?, ?>>();
-	try
-	{
-	    AsyncTask<Object, Object, TickerObject[]> tickersTask = new AsyncTask<Object, Object, TickerObject[]>()
-	    {
-		@Override
-		protected void onPreExecute()
-		{
-		    tasks.add(this);
+		// When swiping between different sections, select the corresponding
+		// tab. We can also use ActionBar.Tab#select() to do this if we have
+		// a reference to the Tab.
+		viewPager
+				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+					@Override
+					public void onPageSelected(int position) {
+						actionBar.setSelectedNavigationItem(position);
+					}
+				});
+
+		// For each of the sections in the app, add a tab to the action bar.
+		for (int i = 0; i < planPagerAdapter.getCount(); i++) {
+			// Create a tab with text corresponding to the page title defined by
+			// the adapter. Also specify this Activity object, which implements
+			// the TabListener interface, as the callback (listener) for when
+			// this tab is selected.
+			actionBar.addTab(actionBar.newTab()
+					.setText(planPagerAdapter.getPageTitle(i))
+					.setTabListener(this));
 		}
-		
-		@Override
-		protected TickerObject[] doInBackground(Object... params)
-		{
-		    try
-		    {
-			ApiResponse resp = API.STANDARD_API.request(ApiAction.TICKER);
-			if(!resp.getSuccess())
-			{
-			    Log.w("Ticker Loader", "Ticker request unsuccesfull");
-			    ticker = -1;
-			    return new TickerObject[0];
-			}
-			ApiResultArray result = (ApiResultArray)resp.getResult();
-			return (TickerObject[])result.getArray(new TickerObject[0]);
-		    } catch (Exception e)
-		    {
+		loadData();
+	}
+
+	private void loadData() {
+		final List<AsyncTask<?, ?, ?>> tasks = new ArrayList<AsyncTask<?, ?, ?>>();
+		try {
+			AsyncTask<Object, Object, TickerObject[]> tickersTask = new AsyncTask<Object, Object, TickerObject[]>() {
+				@Override
+				protected void onPreExecute() {
+					tasks.add(this);
+				}
+
+				@Override
+				protected TickerObject[] doInBackground(Object... params) {
+					try {
+						ApiResponse resp = API.STANDARD_API
+								.request(ApiAction.TICKER);
+						if (!resp.getSuccess()) {
+							Log.w("Ticker Loader",
+									"Ticker request unsuccesfull");
+							ticker = -1;
+							return new TickerObject[0];
+						}
+						ApiResultArray result = (ApiResultArray) resp
+								.getResult();
+						return (TickerObject[]) result
+								.getArray(new TickerObject[0]);
+					} catch (Exception e) {
+						e.printStackTrace();
+						return new TickerObject[0];
+					}
+				}
+
+				@Override
+				protected void onPostExecute(TickerObject[] result) {
+					tasks.remove(this);
+					if (tasks.isEmpty())
+						finishedLoading();
+				}
+			};
+			TickerObject[] toa = tickersTask.execute().get();
+			for (TickerObject to : toa)
+				tickers.add(to);
+			Collections.sort(tickers);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
-			return new TickerObject[0];
-		    }
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void finishedLoading() {
+		LoginActivity.loadingDialog.hide();
+		showTicker();
+	}
+
+	public void getPreferences() {
+
+		try {
+			sharedPref = getSharedPreferences(
+					"com.inf1315.vertretungsplan_preferences", MODE_PRIVATE);
+			tickerToast = sharedPref.getBoolean("pref_toast", true);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.plan_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		case R.id.action_show_ticker:
+			showTicker();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, switch to the corresponding page in
+		// the ViewPager.
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	public void showTicker() {
+
+		if (tickerToast) {
+
+			if (!tickers.isEmpty()) {
+				for (TickerObject to : tickers) {
+					Toast toast = Toast.makeText(getApplicationContext(),
+							to.toString(), Toast.LENGTH_LONG);
+					toast.show();
+				}
+			} else {
+				Toast toast = Toast.makeText(getApplicationContext(),
+						ticker < 0 ? R.string.ticker_loading_error
+								: R.string.no_ticker,
+						ticker < 0 ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		}
+
+		else {
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+			String message = "";
+			for (int i = 0; i < tickers.size(); i++) {
+
+				if (i == tickers.size() - 1)
+					message = message + tickers.get(i).toString();
+				else
+					message = message + tickers.get(i).toString() + "\n";
+			}
+
+			builder.setMessage(message).setTitle(R.string.ticker_dialog_title);
+
+			builder.setPositiveButton(R.string.ok,
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) {
+
+						}
+
+					});
+
+			AlertDialog dialog = builder.create();
+			dialog.show();
+
+		}
+
+	}
+
+	public class PlanPagerAdapter extends FragmentPagerAdapter {
+
+		public PlanPagerAdapter(FragmentManager fm) {
+			super(fm);
 		}
 
 		@Override
-		protected void onPostExecute(TickerObject[] result)
-		{
-		    tasks.remove(this);
-		    if (tasks.isEmpty())
-			finishedLoading();
+		public Fragment getItem(int position) {
+			Fragment fragment = new PlanFragment();
+			Bundle args = new Bundle();
+			boolean isTabToday = (position == 0) ? true : false;
+			args.putBoolean(PlanFragment.ARG_TODAY, isTabToday);
+			fragment.setArguments(args);
+			return fragment;
 		}
-	    };
-	    TickerObject[] toa = tickersTask.execute().get();
-	    for(TickerObject to : toa)
-		tickers.add(to);
-	    Collections.sort(tickers);
-	} catch (InterruptedException e)
-	{
-	    e.printStackTrace();
-	} catch (ExecutionException e)
-	{
-	    e.printStackTrace();
+
+		@Override
+		public int getCount() {
+			// There are 2 Tabs
+			return 2;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+			case 0:
+				return getResources().getString(R.string.today);
+			case 1:
+				return getResources().getString(R.string.tomorrow);
+			}
+			return null;
+		}
 	}
-    }
-    
-    private void finishedLoading()
-    {
-	LoginActivity.loadingDialog.hide();
-	showTicker();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	MenuInflater inflater = getMenuInflater();
-	inflater.inflate(R.menu.plan_activity_actions, menu);
-	return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-	switch (item.getItemId())
-	{
-	    case android.R.id.home:
-		NavUtils.navigateUpFromSameTask(this);
-		return true;
-	    case R.id.action_show_ticker:
-		showTicker();
-		return true;
-	}
-	return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-    {
-	// When the given tab is selected, switch to the corresponding page in
-	// the ViewPager.
-	viewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-    {}
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-    {}
-
-    public void showTicker()
-    {
-	if (!tickers.isEmpty())
-	{
-	    for(TickerObject to : tickers)
-	    {
-		Toast toast = Toast.makeText(getApplicationContext(), to.toString(), Toast.LENGTH_LONG);
-		toast.show();
-	    }
-	} else
-	{
-	    Toast toast = Toast.makeText(getApplicationContext(),
-		ticker < 0 ? R.string.ticker_loading_error : R.string.no_ticker,
-		ticker < 0 ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
-	    toast.show();
-	}
-
-    }
-
-    public class PlanPagerAdapter extends FragmentPagerAdapter
-    {
-
-	public PlanPagerAdapter(FragmentManager fm)
-	{
-	    super(fm);
-	}
-
-	@Override
-	public Fragment getItem(int position)
-	{
-	    Fragment fragment = new PlanFragment();
-	    Bundle args = new Bundle();
-	    boolean isTabToday = (position == 0) ? true : false;
-	    args.putBoolean(PlanFragment.ARG_TODAY, isTabToday);
-	    fragment.setArguments(args);
-	    return fragment;
-	}
-
-	@Override
-	public int getCount()
-	{
-	    // There are 2 Tabs
-	    return 2;
-	}
-
-	@Override
-	public CharSequence getPageTitle(int position)
-	{
-	    switch (position)
-	    {
-		case 0:
-		    return getResources().getString(R.string.today);
-		case 1:
-		    return getResources().getString(R.string.tomorrow);
-	    }
-	    return null;
-	}
-    }
 
 }
