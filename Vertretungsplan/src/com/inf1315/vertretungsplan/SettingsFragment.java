56db83ba55d29236bfde9a70942255aa2bef1168
LeMuecke <simonklimaschka@t-online.de>
2014-01-21 21:27:52 +0100

    Code Cleanup 2

--- Vertretungsplan/src/com/inf1315/vertretungsplan/SettingsFragment.java
+++ Vertretungsplan/src/com/inf1315/vertretungsplan/SettingsFragment.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.content.Context;
 
 public class SettingsFragment extends PreferenceFragment implements
 		OnSharedPreferenceChangeListener {
