5f03f7a92ca9a9c931ccbf5673b8a1670a0db8fa
LeMuecke <simonklimaschka@t-online.de>
2014-02-04 20:49:26 +0100

    Added BootReceiver and PlanSync

---
+++ Vertretungsplan/src/com/inf1315/vertretungsplan/BootReceiver.java
@@ -0,0 +1,32 @@
+package com.inf1315.vertretungsplan;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction().equals("ACTION.BOOT_COMPLETED")) {
+			// Not received, but else doesn't do anything either
+			Log.e("BootReceiver", "BootReceiver started!");
+			AlarmManager alarmMgr = (AlarmManager) context
+					.getSystemService(Context.ALARM_SERVICE);
+			Intent serviceIntent = new Intent(context, PullPlanService.class);
+			PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0,
+					serviceIntent, 0);
+			context.startService(serviceIntent);
+			alarmMgr.setInexactRepeating(
+					AlarmManager.ELAPSED_REALTIME_WAKEUP, // TODO: Implement
+															// time-change
+					AlarmManager.INTERVAL_HOUR, AlarmManager.INTERVAL_HOUR,
+					alarmIntent);
+		}
+
+	}
+
+}
---
+++ Vertretungsplan/src/com/inf1315/vertretungsplan/PullPlanService.java
@@ -0,0 +1,71 @@
+package com.inf1315.vertretungsplan;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.inf1315.vertretungsplan.api.API;
+import com.inf1315.vertretungsplan.api.AllObject;
+import com.inf1315.vertretungsplan.api.ApiAction;
+import com.inf1315.vertretungsplan.api.ApiResponse;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class PullPlanService extends IntentService {
+
+	private String username;
+	private String password;
+	private String token;
+
+	public PullPlanService(String name) { // Auto generated
+		super(name);
+
+		Log.e("TROL", "SUCCESS");
+
+		this.username = API.DATA.userInfo.username;
+		this.token = API.DATA.getToken();
+	}
+
+	public AllObject download() {
+		// TODO: Do a cleanup
+
+		try {
+			ApiResponse resp;
+			if (token == null)
+				resp = API.STANDARD_API.request(ApiAction.ALL, "u=" + username,
+						"pass=" + password);
+			else
+				resp = API.STANDARD_API.request(ApiAction.ALL, "u=" + username,
+						"sync=true", "token=" + token);
+			if (!resp.getSuccess()) {
+				Log.w("All Loader", "Loading unsuccesfull");
+				return null;
+			}
+			if (!resp.getChanged()) {
+				API.DATA.timestamp = System.currentTimeMillis() / 1000L;
+				SimpleDateFormat sdf = new SimpleDateFormat(
+						"dd.MM.yyyy HH:mm:ss");
+				API.DATA.timeString = sdf.format(new Date());
+				return API.DATA;
+			}
+			AllObject ao = (AllObject) resp.getResult();
+			ao.hash = resp.getHash();
+			ao.setToken(resp.getToken());
+			return ao;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	@Override
+	protected void onHandleIntent(Intent workIntent) {
+
+		API.DATA = download();
+
+	}
+
+}
